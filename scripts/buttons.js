319ac719933e8323c0d9358a71dd75412e2e66bd
aergnist <basillicum@basillicum.net>
2017-02-10 16:54:57 +0100

    Dropmeny v1.1
    Javascript fikset. Ikon og generelt utseende må fikses.

--- scripts/buttons.js
+++ scripts/buttons.js
@@ -8,32 +8,10 @@ function loggUtInn() {
 }
 
 function sprettMeny() {
-	var mainbarItems = document.getElementsByClassName("mainbar")[0].getElementsByTagName("a");
-	var kapheItems = document.getElementById("kaphe").getElementsByTagName("a");
-
-	var menu = "<ul>";
-	for(var x = 0; x < mainbarItems.length; x++) {
-		menu += "<li>" + mainbarItems[x] + "</li>";
+	var mobildrop = document.getElementById("mobildrop");
+	if(mobildrop.style.display !== "flex") {
+		mobildrop.style.display = "flex";
+	} else if(mobildrop.style.display !== "none") {
+		mobildrop.style.display = "none";
 	}
-	for(var x = 0; x < kapheItems.length; x++) {
-		menu += "<li>" + kapheItems[x] + "</li>";
-	}
-	menu += "</ul>";
-
-	var drop = document.getElementById("mobildrop");
-	drop.innerHTML = menu;
-}
-
-function mm_showMenu(menuName) // , alwaysVisState = optional. 1=on, 0=off
-{
-  var menuNum = getMenuByName(menuName);
-  if (arguments.length > 1)_m[menuNum][7] = arguments[1];
-  menuDisplay(menuNum, 1);
-}
-
-function mm_hideMenu(menuName) // , alwaysVisState = optional. 1=on, 0=off
-{
-  var menuNum = getMenuByName(menuName);
-  if (arguments.length > 1)_m[menuNum][7] = arguments[1];
-  menuDisplay(menuNum, 0);
 }

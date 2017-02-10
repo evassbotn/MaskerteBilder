ba831c92c48391dea639c4910132bd74cebe0543
aergnist <basillicum@basillicum.net>
2017-02-10 16:24:30 +0100

    Dropmeny
    Dropmeny OK, kan forbedres. Javascript må lages for å hente den frem og
    skjule den igjen.

--- scripts/buttons.js
+++ scripts/buttons.js
@@ -8,5 +8,32 @@ function loggUtInn() {
 }
 
 function sprettMeny() {
+	var mainbarItems = document.getElementsByClassName("mainbar")[0].getElementsByTagName("a");
+	var kapheItems = document.getElementById("kaphe").getElementsByTagName("a");
+
+	var menu = "<ul>";
+	for(var x = 0; x < mainbarItems.length; x++) {
+		menu += "<li>" + mainbarItems[x] + "</li>";
+	}
+	for(var x = 0; x < kapheItems.length; x++) {
+		menu += "<li>" + kapheItems[x] + "</li>";
+	}
+	menu += "</ul>";
+
 	var drop = document.getElementById("mobildrop");
+	drop.innerHTML = menu;
+}
+
+function mm_showMenu(menuName) // , alwaysVisState = optional. 1=on, 0=off
+{
+  var menuNum = getMenuByName(menuName);
+  if (arguments.length > 1)_m[menuNum][7] = arguments[1];
+  menuDisplay(menuNum, 1);
+}
+
+function mm_hideMenu(menuName) // , alwaysVisState = optional. 1=on, 0=off
+{
+  var menuNum = getMenuByName(menuName);
+  if (arguments.length > 1)_m[menuNum][7] = arguments[1];
+  menuDisplay(menuNum, 0);
 }
